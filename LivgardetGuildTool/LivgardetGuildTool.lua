98cdfb56b4912623e5e04e2c60aeed945f606561
Zand3rs <anders.j@me.com>
2021-08-10 21:26:19 +0200

    rework of certain parts

--- LivgardetGuildTool/LivgardetGuildTool.lua
+++ LivgardetGuildTool/LivgardetGuildTool.lua
@@ -3,27 +3,71 @@ LivgardetGuildTool = {
     name = "LivgardetGuildTool",
     addonName = "Livgardet Guild Tool",
     displayName = "|cea4e49Livgardet|r |c40c0f0Guild Tool|r",
-    defaults = {
-        showChatIcon = true,
-        improveDialog = false,
-        dontReadBooks = false,
-  --      luaError = 1,
-    },
     panel = nil,
     chatIcon = nil
 }
 
-local SV
+local LG
 local IconDiscord = "|t25:25:esoui/art/help/help_tabicon_cs_up.dds|t"
 local IconWeb= "|t25:25:esoui/art/tutorial/help_tabicon_tutorial_up.dds|t"
 local IconOpt= "|t25:25:esoui/art/chatwindow/chat_options_up.dds|t"
 local IconGrpTool = "|t25:25:esoui/art/mainmenu/menubar_group_up.dds|t"
 local IconGHouse = "|t25:25:esoui/art/mainmenu/menubar_guilds_up.dds|t"
 
+local defaults = { 
+    showChatIcon = true,
+    improveDialog = false,
+    dontReadBooks = false,
+    nonstopHarvest = false, 
+    hideTopBar = false, 
+    NoCostTravel = false, 
+}
+
+-- FAST TRAVEL CONFIRMATION.
+local function fasterTraveling()
+    local function ShowDialog_NoCostTravel(name, data) 
+        if name == "FAST_TRAVEL_CONFIRM" or name == "RECALL_CONFIRM" then 
+            if LG.NoCostTravel then 
+                FastTravelToNode(data.nodeIndex) 
+                ZO_Dialogs_ReleaseDialog("FAST_TRAVEL_CONFIRM") 
+                return true 
+                end 
+            end 
+        end 
+    ZO_PreHook("ZO_Dialogs_ShowDialog", ShowDialog_NoCostTravel)
+end
+
+-- Hide The Compass
+local function hidetop()
+    if LG.hideTopBar then 
+        ZO_Compass:SetAlpha(0) 
+        ZO_CompassContainer:SetAlpha(0)
+        ZO_CompassFrameCenter:SetHidden(true) 
+        ZO_CompassFrameLeft:SetHidden(true) 
+        ZO_CompassFrameRight:SetHidden(true)
+    else
+        ZO_Compass:SetAlpha(1) 
+        ZO_CompassContainer:SetAlpha(1) 
+        ZO_CompassFrameCenter:SetHidden(false) 
+        ZO_CompassFrameLeft:SetHidden(false) 
+        ZO_CompassFrameRight:SetHidden(false) 
+    end
+end
+
+-- harvest no interrupt
+local function DontInterruptHarvesting()
+	local function Show_Hook(self)
+		if LG.nonstopHarvest then
+			EndPendingInteraction()
+			self:OnShown()
+			return true
+		end
+	end
+	ZO_PreHook(END_IN_WORLD_INTERACTIONS_FRAGMENT, "Show", Show_Hook)
+end
+
 -- Removes the book from view when reading --
-
 local function DontReadBooks()
-
 	local function OnShowBook(eventCode, title, body, medium, showTitle)
 		local willShow = LORE_READER:Show(title, body, medium, showTitle)
 		if willShow then
@@ -35,7 +79,7 @@ local function DontReadBooks()
 	local function OnDontShowBook()
 		EndInteraction(INTERACTION_BOOK)
 	end
-	if SV.dontReadBooks then
+	if LG.dontReadBooks then
 		LORE_READER.control:UnregisterForEvent(EVENT_SHOW_BOOK)
 		LORE_READER.control:RegisterForEvent(EVENT_SHOW_BOOK, OnDontShowBook)
 	else
@@ -45,6 +89,51 @@ local function DontReadBooks()
 end
 
 
+-- Removes the Mail delete confirmation if mail is empty. Does not remove attachments. | /esoui/ingame/mail/keyboard/mailinbox_keyboard.lua
+ZO_PreHook(MAIL_INBOX, "Delete", function(self)
+	if LivgardetGuildTool.db.skipMailDeletionPrompt and self.mailId and self:IsMailDeletable() then
+		local numAttachments, attachedMoney = GetMailAttachmentInfo(self.mailId)
+		if numAttachments == 0 and attachedMoney == 0 then
+			self:ConfirmDelete(self.mailId)
+			return true
+		end
+	end
+end)
+
+-- Removes the Crafting Improvement confirm box --
+local function ImproveDialog()
+        local function ShowDialog_improve(name, data)
+		if name == "CONFIRM_IMPROVE_ITEM" or name == "CONFIRM_IMPROVE_LOCKED_ITEM" or name == "GAMEPAD_CONFIRM_IMPROVE_LOCKED_ITEM" then
+			if LG.improveDialog then
+				ImproveSmithingItem(data.bagId, data.slotIndex, data.boostersToApply)
+				return true
+			end
+		end
+	end
+	ZO_PreHook("ZO_Dialogs_ShowDialog", ShowDialog_improve)
+end
+
+-- Guild Invite from chat function
+local function AddPlayerToGuild(name, guildid, guildname) 
+    d(zo_strformat(GINV_GUILDINVITED, name, guildname)) 
+    GuildInvite(guildid, name) 
+end 
+local CreateChatMenuItem = CHAT_SYSTEM.ShowPlayerContextMenu 
+CHAT_SYSTEM.ShowPlayerContextMenu = function(self, name, rawName, ...) 
+    CreateChatMenuItem(self, name, rawName, ...) 
+    for i = 1, GetNumGuilds() do 
+        local gid = GetGuildId(i) 
+        if DoesPlayerHaveGuildPermission(gid, GUILD_PERMISSION_INVITE) then 
+            local guildName = GetGuildName(gid) 
+            AddMenuItem(zo_strformat(GINV_GUILDINVITE, guildName), function() AddPlayerToGuild(name, gid, guildName) end) 
+        end 
+    end 
+    if ZO_Menu_GetNumMenuItems() > 0 then 
+        ShowMenu() 
+    end 
+end
+
+-- Initialize the menu stuff.
 function LivgardetGuildTool:InitializeMenu()
     local LAM2 = LibAddonMenu2
 
@@ -53,91 +142,92 @@ function LivgardetGuildTool:InitializeMenu()
         name = self.addonName,
         displayName = self.displayName,
         author = "Zand3rs",
-        version = "1.1",
+        version = "1.5",
         slashCommand = "/livgardet",
         website = "https://www.esoui.com",
         registerForRefresh = true,
         registerForDefaults = true,
     }
 
+-- Options for the settings page. 
     local optionsTable = {
-     
-        {
-            type = "header",
-            name = GetString(LIVGARDET_SETTINGS_HEADER_GENERAL),
-            width = "half",
+        { 
+            type = "header", name = GetString(LIVGARDET_SETTINGS_HEADER_GENERAL), width = "half", 
         },
-        {
-            type = "checkbox",
-            name = GetString(LIVGARDET_SETTINGS_CHAT_ICON),
-            tooltip = GetString(LIVGARDET_SETTINGS_CHAT_ICON_TT),
+        { -- SHOW OR HIDE CHAT ICON TO OPEN MENU
+            type = "checkbox", name = GetString(LIVGARDET_SETTINGS_CHAT_ICON), 
+            tooltip = GetString(LIVGARDET_SETTINGS_CHAT_ICON_TT), 
             getFunc = function() return self.db.showChatIcon end,
-            setFunc = function( show )
-                self.db.showChatIcon = show
-                self:ShowChatIcon(show)
-            end,
+            setFunc = function( show ) self.db.showChatIcon = show self:ShowChatIcon(show) end,
         },
         {
-            type = 'header',
-            name = 'Quality of life settings',
-            width = 'full',
+            type = 'header', name = 'Quality of life settings', width = 'full',
         },
-        {
+        { -- SKIP CONFIRM PROMPT WHEN ERASING MAIL
             type = "checkbox",
             name = GetString(LIVGARDET_SETTINGS_MAIL_DELETION),
             tooltip = GetString(LIVGARDET_SETTINGS_MAIL_DELETION_TT),
             getFunc = function() return self.db.skipMailDeletionPrompt end,
-            setFunc = function( skip )
-                self.db.skipMailDeletionPrompt = skip
-            end,
+            setFunc = function( skip ) self.db.skipMailDeletionPrompt = skip end,
         },
-        {
+        { -- HIDE DIALOG WHEN IMPROVING ITEMS
 			type = "checkbox",
 			name = GetString(LIVGARDET_SETTINGS_CONFIRM_IMPROVE),
             tooltip = GetString(LIVGARDET_SETTINGS_CONFIRM_IMPROVE_TT),
-			getFunc = function() return SV.improveDialog end,
-			setFunc = function(value) SV.improveDialog = value 
-          end,
+			getFunc = function() return LG.improveDialog end,
+			setFunc = function(value) LG.improveDialog = value end,
 		},
-        {
+        { -- HIDE BOOK WHEN READING
 			type = "checkbox",
 			name = GetString(LIVGARDET_SETTINGS_CONFIRM_NOBOOK),
             tooltip = GetString(LIVGARDET_SETTINGS_CONFIRM_NOBOOK_TT),
-			getFunc = function() return SV.dontReadBooks end,
-			setFunc = function(value) SV.dontReadBooks = value
-				DontReadBooks()
-			end,
+			getFunc = function() return LG.dontReadBooks end,
+			setFunc = function(value) LG.dontReadBooks = value DontReadBooks() end,
 		},
+
+        { -- HIDE COMPASS
+            type = 'checkbox',
+            name = GetString(LIVGARDET_SETTINGS_COMPASS),
+            tooltip = GetString(LIVGARDET_SETTINGS_COMPASS_TT),
+            getFunc = function() return LG.hideTopBar end,
+            setFunc = function(value) LG.hideTopBar = value; hidetop() end,
+            default = hideTopBar,
+         },
+
+         { -- NO COST TRAVEL
+            type = 'checkbox',
+            name = GetString(LIVGARDET_SETTINGS_CONFIRM_FAST_TRAVEL),
+            tooltip = GetString(LIVGARDET_SETTINGS_CONFIRM_FAST_TRAVEL_TT),
+            getFunc = function() return LG.NoCostTravel end,
+            setFunc = function(value) LG.NoCostTravel = value end,
+         }, 
     } 
-    
     self.panel = LAM2:RegisterAddonPanel(self.name .. "Options", panelData)
     LAM2:RegisterOptionControls(self.name .. "Options", optionsTable)
 end
 
-function LivgardetGuildTool:InitializeChatIcon()
-    local ptoGHall = WINDOW_MANAGER:CreateControl("LivgardetGuildTool1", ZO_ChatWindow, CT_BUTTON)
-    ptoGHall:SetDimensions(22, 22)
-    ptoGHall:SetAnchor(TOPLEFT, ZO_ChatOptionsSectionLabel, TOPRIGHT, -100, 10)
-    ptoGHall:SetHandler("OnMouseEnter", function(control)
-        InitializeTooltip(InformationTooltip, control)
-        SetTooltipText(InformationTooltip, "Livgardet", 1, 1, 1, LEFT, MODIFY_TEXT_TYPE_NONE, TEXT_ALIGN_CENTER, true)
+-- Chat icon Picture and menu configuration
+function LivgardetGuildTool:InitializeChatIcon() 
+    local LivgHall = WINDOW_MANAGER:CreateControl("LivgardetGuildTool1", ZO_ChatWindow, CT_BUTTON) 
+    LivgHall:SetDimensions(22, 22)
+    LivgHall:SetAnchor(TOPLEFT, ZO_ChatOptionsSectionLabel, TOPLEFT, 200, 13)
+    LivgHall:SetHandler("OnMouseEnter", function(control) 
+        InitializeTooltip(InformationTooltip, control) 
+        SetTooltipText(InformationTooltip, "Livgardet", 1, 1, 1, LEFT, MODIFY_TEXT_TYPE_NONE, TEXT_ALIGN_CENTER, true) 
+    end) 
+    LivgHall:SetHandler("OnMouseExit", function(_) ClearTooltip(InformationTooltip) 
     end)
-    ptoGHall:SetHandler("OnMouseExit", function(_)
-        ClearTooltip(InformationTooltip)
-    end)
-    ptoGHall:SetNormalTexture("LivgardetGuildTool/imgs/livgardeteso.dds")
-    ptoGHall:SetPressedTexture("LivgardetGuildTool/imgs/livgardeteso.dds")
-    ptoGHall:SetMouseOverTexture("LivgardetGuildTool/imgs/livgardeteso.dds")
-
-    ptoGHall:SetHandler("OnClicked", function(...)
-        local entries = {
-            {
-                label = GetString(LIVGARDET_PORT_GUILDHOUSE),
-                callback = function()
-                    LivgardetGuildTool:PortToHouse("@Nsaje", 71, GetString(LIVGARDET_CHAT_GUILDHOUSE))
-
-                end,
-            },
+    LivgHall:SetNormalTexture("LivgardetGuildTool/imgs/livgardeteso.dds")
+    LivgHall:SetPressedTexture("LivgardetGuildTool/imgs/livgardeteso.dds")
+    LivgHall:SetMouseOverTexture("LivgardetGuildTool/imgs/livgardeteso.dds")
+    LivgHall:SetHandler("OnClicked", function(...) 
+        local entries = { 
+            { 
+                label = GetString(LIVGARDET_PORT_GUILDHOUSE), 
+                callback = function() 
+                    LivgardetGuildTool:PortToHouse("@Nsaje", 66, GetString(LIVGARDET_CHAT_GUILDHOUSE)) 
+                end, 
+            }, 
             {
                 label = "-",
             },
@@ -157,7 +247,6 @@ function LivgardetGuildTool:InitializeChatIcon()
                 end,
             }
         }
-
         local entries2 = {
             {
                 label = GetString(LIVGARDET_GUILD_MESSAGE),
@@ -166,17 +255,14 @@ function LivgardetGuildTool:InitializeChatIcon()
                 end,
             }
         }
-
         ClearMenu()
         AddCustomSubMenuItem(IconGHouse..GetString(LIVGARDET_GUILD_HOUSES), entries, normalColor)
         AddCustomMenuItem("-", function()
-        end)
-
+        end) 
         AddCustomSubMenuItem(IconGrpTool..GetString(LIVGARDET_GUILD_TOOLS), entries2, normalColor)
-
         AddCustomMenuItem("-", function()
         end)
-        AddCustomMenuItem(IconDiscord..GetString(LIVGARDET_OUR_DISCORD), function()
+        AddCustomMenuItem(IconDiscord..GetString(LIVGARDET_OUR_DISCORD), function() 
             RequestOpenUnsafeURL("https://discord.gg/xNjWbaRjn3")
         end)
         AddCustomMenuItem("-", function()
@@ -186,15 +272,16 @@ function LivgardetGuildTool:InitializeChatIcon()
         end)
         AddCustomMenuItem("-", function()
         end)
-        AddCustomMenuItem(IconOpt..GetString(LIVGARDET_BUTTON_SETTINGS), function()
-            LAM2:OpenToPanel(self.panel)
-        end)
+
+-- COMMENTED OUT THE OPEN SETTINGS CHOICE BECAUSE IT BUGS OUT FOR NOW !!!
+--      AddCustomMenuItem(IconOpt..GetString(LIVGARDET_BUTTON_SETTINGS), function() 
+--          LAM2:OpenToPanel(self.panel)
+--      end)
         ShowMenu()
     end)
-    self.chatIcon = ptoGHall
+    self.chatIcon = LivgHall
 end
-
-    function LivgardetGuildTool:PortToHouse(name, houseId, message)
+function LivgardetGuildTool:PortToHouse(name, houseId, message)
         if (GetDisplayName() == name) then
             RequestJumpToHouse(houseId)
         else
@@ -205,76 +292,26 @@ end
     function LivgardetGuildTool:ShowChatIcon(show)   
     self.chatIcon:SetHidden(not show)
 end
-
 function LivgardetGuildTool:Initialize()
     self.db = ZO_SavedVars:NewAccountWide("LivgardetSavedVars", 1, nil, self.defaults)
-
+--    LG = ZO_SavedVars:NewAccountWide("LivgardetSavedVars", 1, defaults) 
     self:InitializeMenu()
     self:InitializeChatIcon()
-
     self:ShowChatIcon(self.db.showChatIcon)
 end
 
--- Guild Inviter
-
-local function AddPlayerToGuild(name, guildid, guildname) 
-    d(zo_strformat(GINV_GUILDINVITED, name, guildname)) 
-    GuildInvite(guildid, name) 
-end 
-
-local CreateChatMenuItem = CHAT_SYSTEM.ShowPlayerContextMenu 
-CHAT_SYSTEM.ShowPlayerContextMenu = function(self, name, rawName, ...) 
-    CreateChatMenuItem(self, name, rawName, ...) 
-    for i = 1, GetNumGuilds() do 
-        local gid = GetGuildId(i) 
-        if DoesPlayerHaveGuildPermission(gid, GUILD_PERMISSION_INVITE) then 
-            local guildName = GetGuildName(gid) 
-            AddMenuItem(zo_strformat(GINV_GUILDINVITE, guildName), function() AddPlayerToGuild(name, gid, guildName) end) 
-        end 
-    end 
-    if ZO_Menu_GetNumMenuItems() > 0 then 
-        ShowMenu() 
-    end 
-end
-
--- Removes the Mail delete confirmation if mail is empty. Does not remove attachments. | /esoui/ingame/mail/keyboard/mailinbox_keyboard.lua
-
-ZO_PreHook(MAIL_INBOX, "Delete", function(self)
-	if LivgardetGuildTool.db.skipMailDeletionPrompt and self.mailId and self:IsMailDeletable() then
-		local numAttachments, attachedMoney = GetMailAttachmentInfo(self.mailId)
-		if numAttachments == 0 and attachedMoney == 0 then
-			self:ConfirmDelete(self.mailId)
-			return true
-		end
-	end
-end)
-
--- Removes the Crafting Improvement confirm box --
-
-local function HookImproveDialog()
-	local function ShowDialog_Hook(name, data)
-		if name == "CONFIRM_IMPROVE_ITEM" or name == "CONFIRM_IMPROVE_LOCKED_ITEM" or name == "GAMEPAD_CONFIRM_IMPROVE_LOCKED_ITEM" then
-			if SV.improveDialog then
-				ImproveSmithingItem(data.bagId, data.slotIndex, data.boostersToApply)
-				return true
-			end
-		end
-	end
-	ZO_PreHook("ZO_Dialogs_ShowDialog", ShowDialog_Hook)
-end
-
--- EMD OF CODE.. I wish!
-
+-- Do i really need both if i reqrite everything? Ofcourse not  :)
 function LivgardetGuildTool.OnAddOnLoaded(_, addon)
     if addon == LivgardetGuildTool.name then
         EVENT_MANAGER:UnregisterForEvent(ADDON_NAME, event) 
-        SV = ZO_SavedVars:NewAccountWide("LivgardetSavedVars", 1, defaults)
-		
-        HookImproveDialog()
-        DontReadBooks()
+        LG = ZO_SavedVars:NewAccountWide("LivgardetSavedVars", 1, defaults) 
+        ImproveDialog()
+        DontReadBooks() 
+        DontInterruptHarvesting() 
+        hidetop() 
+        fasterTraveling()
 
         LivgardetGuildTool:Initialize()
     end
 end
-
 EVENT_MANAGER:RegisterForEvent(LivgardetGuildTool.name, EVENT_ADD_ON_LOADED, LivgardetGuildTool.OnAddOnLoaded)
--- LivgardetGuildTool/lang/en.lua
+++ LivgardetGuildTool/lang/en.lua
@@ -11,28 +11,49 @@ ZO_CreateStringId("LIVGARDET_OUR_WEBSITE", "Our Website")
 ZO_CreateStringId("LIVGARDET_BUTTON_SETTINGS", "Settings")
 
 -- Settings
-ZO_CreateStringId("LIVGARDET_SETTINGS", "Settings")
-ZO_CreateStringId("LIVGARDET_SETTINGS_HEADER_GENERAL", "General")
+ ZO_CreateStringId("LIVGARDET_SETTINGS", "Settings")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_HEADER_GENERAL", "General")
 
 -- Show the chat icon
-ZO_CreateStringId("LIVGARDET_SETTINGS_CHAT_ICON", "Show chat icon")
-ZO_CreateStringId("LIVGARDET_SETTINGS_CHAT_ICON_TT", "Shows or removes the icon from the chat window")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CHAT_ICON", "Show chat icon")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CHAT_ICON_TT", "Shows or removes the icon from the chat window")
 
 -- Chat Outputs
-ZO_CreateStringId("LIVGARDET_CHAT_GUILDHOUSE", "The journey to the Guild House starts :)")
-ZO_CreateStringId("LIVGARDET_CHAT_PARSEHOUSE", "I hope you brought enough potions ;)")
-ZO_CreateStringId("LIVGARDET_CHAT_SECRETHOUSE", "Coming Soon!!!")
+ ZO_CreateStringId("LIVGARDET_CHAT_GUILDHOUSE", "The journey to the Guild House starts :)")
+ ZO_CreateStringId("LIVGARDET_CHAT_PARSEHOUSE", "I hope you brought enough potions ;)")
+ ZO_CreateStringId("LIVGARDET_CHAT_SECRETHOUSE", "Coming Soon!!!")
 
 -- chat inviter --
-ZO_CreateStringId("GINV_GUILDINVITE", "Invite into \"<<1>>\" guild")
-ZO_CreateStringId("GINV_GUILDINVITED", "Invited <<C:1>> into \"<<2>>\" guild.")
+ ZO_CreateStringId("GINV_GUILDINVITE", "Invite into \"<<1>>\" guild")
+ ZO_CreateStringId("GINV_GUILDINVITED", "Invited <<C:1>> into \"<<2>>\" guild.")
 
--- Quality of life improvements --
-ZO_CreateStringId("LIVGARDET_SETTINGS_MAIL_DELETION", "Disable erase mail confirmation")
-ZO_CreateStringId("LIVGARDET_SETTINGS_MAIL_DELETION_TT", "This function removes the confirmation but only on mail without attachments")
 
-ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_IMPROVE", "Disable Improvement confirmation")
-ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_IMPROVE_TT", "This function removes the confirmation when upgrading gear so be careful.")
+ -- Quality of life improvements --
 
-ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_NOBOOK", "Quick Lorebook reading")
-ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_NOBOOK_TT", "Hides the LoreBook from view when clicking them in world")
+-- Mail confirmation
+ ZO_CreateStringId("LIVGARDET_SETTINGS_MAIL_DELETION", "Disable erase mail confirmation")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_MAIL_DELETION_TT", "This function removes the confirmation but only on mail without attachments")
+
+-- Improve gear confirmation
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_IMPROVE", "Disable Improvement confirmation")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_IMPROVE_TT", "This function removes the confirmation when upgrading gear so be careful.")
+
+-- No confirmation or book on read
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_NOBOOK", "Quick Lorebook reading")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_NOBOOK_TT", "Hides the LoreBook from view when clicking them in world")
+
+-- HIDE COMPASS
+ ZO_CreateStringId("LIVGARDET_SETTINGS_COMPASS", "Remove the compass from UI")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_COMPASS_TT", "This will remove the whole compass from the UI. No reload needed.")
+
+ -- NO COST TRAVEL
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_FAST_TRAVEL", "Disable wayshrine cost confirmation")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_CONFIRM_FAST_TRAVEL_TT", "Activating this will remove the confirmation window that displays the cost when using wayshrines")
+
+
+-- Work in progress!!!
+ ZO_CreateStringId("LIVGARDET_SETTINGS_REPAIR", "Fast repair")
+ ZO_CreateStringId("LIVGARDET_SETTINGS_REPAIR_TT", "Removes the dialog box when repearing gear")
+
+-- EMPTY LINE
+-- ZO_CreateStringId("LIVGARDET_SETTINGS_", "")
